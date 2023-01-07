15fff471d799f0255d8d02e1228b3ae508fa7e8e
Nikhil Nygaard <nik@nygaard.site>
2023-01-07 00:12:34 -0700

    build environment

--- systemd/scripts/fire.sh
+++ systemd/scripts/fire.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /home/drone/development/kindling
-go build -o /home/drone/development/kindling
+go build -o /home/drone/development/kindling -buildvcs=false
 /home/drone/development/kindling/kindling
 cp /home/drone/development/kindling/out.png /var/www/kindle/image.png
