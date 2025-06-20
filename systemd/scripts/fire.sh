f18c572b41013e1c7d5817531abab0032fcd8237
Nikhil Nygaard <nik@nygaard.site>
2025-06-20 11:22:30 -0700

    remove -buildvcs

--- systemd/scripts/fire.sh
+++ systemd/scripts/fire.sh
@@ -5,7 +5,7 @@ set -x
 cd /home/drone/development/kindling
 rm kindling
 rm weather.png
-go build -o /home/drone/development/kindling -buildvcs=false
+go build -o /home/drone/development/kindling
 /home/drone/development/kindling/kindling
 convert /home/drone/development/kindling/weather.png -monochrome -colors 2 -depth 1 -strip png:/var/www/sploo.sh/weather.png
 
